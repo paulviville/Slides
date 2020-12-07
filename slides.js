e6717f389bfd851a4a16a6060a3f5dc4d91fbd5c
paulviville <paul.viville@gmail.com>
2020-12-07 02:11:49 +0100

    added meshes and slides

--- slides.js
+++ slides.js
@@ -5,9 +5,10 @@ import {OrbitControls} from './CMapJS/OrbitsControls.js';
 import {load_graph} from './CMapJS/IO/Graph_Formats/Graph_IO.js' 
 import {load_cmap2, export_cmap2} from './CMapJS/IO/Surface_Formats/CMap2_IO.js' 
 import {load_cmap3} from './CMapJS/IO/Volumes_Formats/CMap3_IO.js' 
-import {tetrahedron_off, icosahedron_off, cube_off, octahedron_off, cactus_off, cactus_scaffold_off, fertility_off, metatron_off} from './off_files.js';
-import {cactus0_mesh, cactus1_mesh, fertility, dinopet, santa, ortho3, cactus, test0_mesh, metatron} from './mesh_files.js';
-import {cactus_simplified_cg, cactus_cg} from './cg_files.js';
+import {cactus_off, cactus_scaffold_off, fertility_off, fertility_scaffold_off} from './Files/off_files.js';
+import {cactus_simplified_cg, cactus_cg, fertility_simplified_cg, fertility_cg} from './Files/cg_files.js';
+import {fertility_mesh} from './Files/fertility_files.js';
+import {cactus0_mesh, cactus1_mesh, cactus_mesh} from './Files/cactus_files.js';
 import {BoundingBox} from './CMapJS/Utils/BoundingBox.js';
 
 
@@ -37,54 +38,73 @@ let mesh_edge_material = new THREE.LineBasicMaterial({
 let cactus_surface = load_cmap2('off', cactus_off);
 let cactus_initial_mesh = load_cmap3('mesh', cactus0_mesh);
 let cactus_surface_fit_mesh = load_cmap3('mesh', cactus1_mesh);
+let cactus_opt_mesh = load_cmap3('mesh', cactus_mesh);
 let cactus_skel = load_graph('cg', cactus_cg);
 let cactus_skel_simple = load_graph('cg', cactus_simplified_cg);
 let cactus_scaffold = load_cmap2('off', cactus_scaffold_off);
-let bb = BoundingBox(cactus_surface.get_attribute(cactus_surface.vertex, "position"))
+let fertility_surface = load_cmap2('off', fertility_off);
+let fertility_skel = load_graph('cg', fertility_cg);
+let fertility_simplified_skel = load_graph('cg', fertility_simplified_cg);
+let fertility_scaffold = load_cmap2('off', fertility_scaffold_off);
+let fertility_vol = load_cmap3('mesh', fertility_mesh);
 
-let axis_x = new THREE.Vector3(1, 0, 0);
-let axis_y = new THREE.Vector3(0, 1, 0);
-let pos = cactus_surface.get_attribute(cactus_surface.vertex, "position");
-cactus_surface.foreach(cactus_surface.vertex, vd => {
-	pos[cactus_surface.cell(cactus_surface.vertex, vd)].sub(bb.mid);
-	pos[cactus_surface.cell(cactus_surface.vertex, vd)].applyAxisAngle(axis_x,-Math.PI/2);
-	pos[cactus_surface.cell(cactus_surface.vertex, vd)].applyAxisAngle(axis_y,-Math.PI/2);
-});	
+// let bb = BoundingBox(cactus_surface_fit_mesh.get_attribute(cactus_surface_fit_mesh.vertex, "position"))
+// console.log(bb)
+export let slide_overview = new Slide(
+	function(input, output)
+	{
+		this.camera = new THREE.PerspectiveCamera(75, input.width / input.height, 0.1, 1000.0);
+		this.camera.position.set(0, 0, 0.8);
 
-pos = cactus_skel.get_attribute(cactus_skel.vertex, "position");
-cactus_skel.foreach(cactus_skel.vertex, vd => {
-	pos[cactus_skel.cell(cactus_skel.vertex, vd)].sub(bb.mid);
-	pos[cactus_skel.cell(cactus_skel.vertex, vd)].applyAxisAngle(axis_x,-Math.PI/2);
-	pos[cactus_skel.cell(cactus_skel.vertex, vd)].applyAxisAngle(axis_y,-Math.PI/2);
-});	
+		this.scene = new THREE.Scene()
+		let ambiant = new THREE.AmbientLight(0xFFFFFF, 0.8);
+		this.scene.add(ambiant);
+		let pointLight0 = new THREE.PointLight(0xFFFFFF, 0.8);
+		pointLight0.position.set(10,8,5);
+		this.scene.add(pointLight0);
 
-pos = cactus_skel_simple.get_attribute(cactus_skel_simple.vertex, "position");
-cactus_skel_simple.foreach(cactus_skel_simple.vertex, vd => {
-	pos[cactus_skel_simple.cell(cactus_skel_simple.vertex, vd)].sub(bb.mid);
-	pos[cactus_skel_simple.cell(cactus_skel_simple.vertex, vd)].applyAxisAngle(axis_x,-Math.PI/2);
-	pos[cactus_skel_simple.cell(cactus_skel_simple.vertex, vd)].applyAxisAngle(axis_y,-Math.PI/2);
-});	
+		this.scene2 = new THREE.Scene()
+		let ambiant2 = new THREE.AmbientLight(0xFFFFFF, 0.8);
+		this.scene2.add(ambiant2);
+		let pointLight02 = new THREE.PointLight(0xFFFFFF, 0.8);
+		pointLight02.position.set(10,8,5);
+		this.scene2.add(pointLight02);
 
-pos = cactus_initial_mesh.get_attribute(cactus_initial_mesh.vertex, "position");
-cactus_initial_mesh.foreach(cactus_initial_mesh.vertex, vd => {
-	pos[cactus_initial_mesh.cell(cactus_initial_mesh.vertex, vd)].sub(bb.mid);
-	pos[cactus_initial_mesh.cell(cactus_initial_mesh.vertex, vd)].applyAxisAngle(axis_x,-Math.PI/2);
-	pos[cactus_initial_mesh.cell(cactus_initial_mesh.vertex, vd)].applyAxisAngle(axis_y,-Math.PI/2);
-});	
+		this.renderer_input = new THREE.WebGLRenderer({
+			canvas: input,
+			antialias: true,
+			alpha: true
+		});	
+		let orbit_controls_input = new OrbitControls(this.camera, this.renderer_input.domElement);
 
-pos = cactus_scaffold.get_attribute(cactus_scaffold.vertex, "position");
-cactus_scaffold.foreach(cactus_scaffold.vertex, vd => {
-	pos[cactus_scaffold.cell(cactus_scaffold.vertex, vd)].sub(bb.mid);
-	pos[cactus_scaffold.cell(cactus_scaffold.vertex, vd)].applyAxisAngle(axis_x,-Math.PI/2);
-	pos[cactus_scaffold.cell(cactus_scaffold.vertex, vd)].applyAxisAngle(axis_y,-Math.PI/2);
-});	
+		this.renderer_output = new THREE.WebGLRenderer({
+			canvas: output,
+			antialias: true,
+			alpha: true
+		});	
+		let orbit_controls_output = new OrbitControls(this.camera, this.renderer_output.domElement);
 
-pos = cactus_surface_fit_mesh.get_attribute(cactus_surface_fit_mesh.vertex, "position");
-cactus_surface_fit_mesh.foreach(cactus_surface_fit_mesh.vertex, vd => {
-	pos[cactus_surface_fit_mesh.cell(cactus_surface_fit_mesh.vertex, vd)].sub(bb.mid);
-	pos[cactus_surface_fit_mesh.cell(cactus_surface_fit_mesh.vertex, vd)].applyAxisAngle(axis_x,-Math.PI/2);
-	pos[cactus_surface_fit_mesh.cell(cactus_surface_fit_mesh.vertex, vd)].applyAxisAngle(axis_y,-Math.PI/2);
-});	
+		this.fertility_surface_renderer = new Renderer(fertility_surface);
+		this.fertility_surface_renderer.faces.create({material: mesh_face_alpha_material}).add(this.scene);
+
+
+		this.graph_renderer = new Renderer(fertility_skel);
+		this.graph_renderer.edges.create({material: mesh_edge_material}).add(this.scene);
+
+		this.map_renderer3 = new Renderer(fertility_vol);
+		this.map_renderer3.volumes.create({material: mesh_face_material}).add(this.scene2);
+		this.map_renderer3.volumes.rescale(0.85);
+
+		this.loop = function(){
+			if(this.running){
+				console.log("running");
+				this.renderer_input.render(this.scene, this.camera);
+				this.renderer_output.render(this.scene2, this.camera);
+				requestAnimationFrame(this.loop.bind(this));
+			}
+		}
+	}
+);
 
 export let slide_process_0 = new Slide(
 	function(surface, skel, skel_simple){
@@ -146,7 +166,6 @@ export let slide_process_0 = new Slide(
 
 		this.loop = function(){
 			if(this.running){
-				// this.camera.disableAll();
 				this.camera.layers.enable(0);
 				this.renderer_surface.render(this.scene, this.camera);
 				this.camera.layers.disable(0);
@@ -164,7 +183,6 @@ export let slide_process_0 = new Slide(
 	}
 );
 
-
 export let slide_process_1 = new Slide(
 	function(scaffold, initial_mesh, surface_fit_mesh){
 		this.camera = new THREE.PerspectiveCamera(75, scaffold.width / scaffold.height, 0.1, 1000.0);
@@ -176,10 +194,8 @@ export let slide_process_1 = new Slide(
 		pointLight.position.set(10,8,5);
 		ambiantLight.layers.enable(1);
 		ambiantLight.layers.enable(2);
-		// ambiantLight.layers.enable(3);
 		pointLight.layers.enable(1);
 		pointLight.layers.enable(2);
-		// pointLight.layers.enable(3);
 		this.scene.add(ambiantLight);
 		this.scene.add(pointLight);
 
@@ -227,20 +243,6 @@ export let slide_process_1 = new Slide(
 		let orbit_controls_surface_fit_mesh = new OrbitControls(this.camera, this.renderer_surface_fit_mesh.domElement);
 		
 
-		// this.renderer_skel = new THREE.WebGLRenderer({
-		// 	canvas: skel,
-		// 	antialias: true,
-		// 	alpha: true
-		// });	
-		// let orbit_controls_skel = new OrbitControls(this.camera, this.renderer_skel.domElement);
-		
-		// this.renderer_skel_simple = new THREE.WebGLRenderer({
-		// 	canvas: skel_simple,
-		// 	antialias: true,
-		// 	alpha: true
-		// });	
-		// let orbit_controls_skel_simple = new OrbitControls(this.camera, this.renderer_skel_simple.domElement);
-
 		this.loop = function(){
 			if(this.running){
 				this.camera.layers.disableAll();
@@ -252,16 +254,41 @@ export let slide_process_1 = new Slide(
 				this.renderer_initial_mesh.render(this.scene, this.camera);
 				this.camera.layers.disable(2);
 				this.camera.layers.enable(3);
-
 				this.renderer_surface_fit_mesh.render(this.scene, this.camera);
-				// this.camera.layers.disable(2);
-				// this.camera.layers.enable(1);
-				// this.camera.layers.enable(2);
-				// this.renderer_skel.render(this.scene, this.camera);
-				// this.camera.layers.disable(2);
-				// this.camera.layers.enable(3);
-				// this.renderer_skel_simple.render(this.scene, this.camera);
-				// this.camera.layers.disable(3);
+
+				requestAnimationFrame(this.loop.bind(this));
+			}
+		}
+
+	}
+);
+
+export let slide_process_2 = new Slide(
+	function(result){
+		this.camera = new THREE.PerspectiveCamera(75, result.width / result.height, 0.1, 1000.0);
+		this.camera.position.set(0, 0, 0.8);
+
+		this.scene = new THREE.Scene();
+		let ambiantLight = new THREE.AmbientLight(0xFFFFFF, 0.8);
+		let pointLight = new THREE.PointLight(0xFFFFFF, 0.8);
+		pointLight.position.set(10,8,5);
+		this.scene.add(ambiantLight);
+		this.scene.add(pointLight);
+
+		this.initial_mesh_renderer = new Renderer(cactus_opt_mesh);
+		this.initial_mesh_renderer.volumes.create({ material: mesh_face_alpha_material}).add(this.scene);
+		this.initial_mesh_renderer.volumes.rescale(0.8);
+
+		this.renderer_result = new THREE.WebGLRenderer({
+			canvas: result,
+			antialias: true,
+			alpha: true
+		});
+		let orbit_controls  = new OrbitControls(this.camera, this.renderer_result.domElement);
+
+		this.loop = function(){
+			if(this.running){
+				this.renderer_result.render(this.scene, this.camera);
 				requestAnimationFrame(this.loop.bind(this));
 			}
 		}
